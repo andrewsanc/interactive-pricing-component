8fe929990cb18846510e774066ab7cf80ce1b913
TB831 <druu831@gmail.com>
2024-02-20 15:29:33 -0800

    fix. boilerplate cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <>
-      <h1 className="text-xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="block">
+      <div>hello world</div>
+    </div>
   );
 }
