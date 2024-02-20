ce35c92cddfa87d1d25e80777ae23caac7ed263b
TB831 <druu831@gmail.com>
2024-02-20 15:41:09 -0800

    feat. added bg slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <div className="block">
-      <div>hello world</div>
+    <div className="block relative h-screen">
+      <div className="absolute h-1/2 w-full bg-gray-100 rounded-bl-[40%]"></div>
     </div>
   );
 }
